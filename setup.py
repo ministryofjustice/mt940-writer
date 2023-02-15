6ed88ad2b2ec1aa1b0ab446c3bda1eec3cf3a0b5
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-02-15 10:34:22 +0000

    Migrate to declarative build system

--- setup.py
+++ setup.py
@@ -1,48 +1,10 @@
 #!/usr/bin/env python
-import importlib
-import os
 import sys
 import warnings
 
 from setuptools import setup
 
 if sys.version_info[0:2] < (3, 7):
-    warnings.warn('This package is tested with Python version 3.7+', stacklevel=1)
+    warnings.warn('This package is only tested on Python version 3.7+', stacklevel=1)
 
-root_path = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(root_path, 'README.rst')) as readme:
-    README = readme.read()
-
-package_info = importlib.import_module('mt940_writer')
-
-setup(
-    name='mt940-writer',
-    version=package_info.__version__,
-    author=package_info.__author__,
-    author_email='dev@digital.justice.gov.uk',
-    url='https://github.com/ministryofjustice/mt940-writer',
-    py_modules=['mt940_writer'],
-    include_package_data=True,
-    license='MIT',
-    description='Writer for MT-940 bank statements',
-    long_description=README,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    python_requires='>=3.6',  # looser requirement than what's tested
-    install_requires=[],
-    tests_require=[],
-    test_suite='tests',
-)
+setup()
